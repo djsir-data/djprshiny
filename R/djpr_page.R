b095d62809ca05d81e818eb1b8428248002b6743
Matt Cowgill <mattcowgill@gmail.com>
2021-05-10 10:51:04 +1000

    export djpr_page

--- R/djpr_page.R
+++ R/djpr_page.R
@@ -1,5 +1,18 @@
+#' Create a `shiny::fluidPage()` with the DJPR theme
+#'
+#' Structured as a `navbarPage()`. Provides option to include a table of
+#' contents, which must be further defined within individual `tabPanel`(s) -
+#' see `?toc_row`.
+#'
+#' @param title Dashboard title, such as "DJPR Labour Dashboard"
+#' @param ... content to include within the `navbarPage()`. The content
+#' should typically be one or more `tabPanel`s.
+#'
+#' @export
+
 djpr_page <- function(title,
                       ...) {
+
   shiny::fluidPage(
 
     theme = djpr_shiny_theme(),
