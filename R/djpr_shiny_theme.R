3718b215bc6eda260718b4cca91d14709b549b11
Matt Cowgill <mattcowgill@gmail.com>
2021-05-14 11:40:21 +1000

    add round2

--- R/djpr_shiny_theme.R
+++ R/djpr_shiny_theme.R
@@ -36,8 +36,10 @@ djpr_shiny_theme <- function(...) {
     ),
     `enable-shadows` = TRUE,
   ) %>%
-    bslib::bs_add_variables("navbar-padding-y" = "spacer / 2",
-                            "navbar-brand-font-size" = "1rem") %>%
+    bslib::bs_add_variables(
+      "navbar-padding-y" = "spacer / 2",
+      "navbar-brand-font-size" = "1rem"
+    ) %>%
     # Hacky way to add spacing around brand:
     # https://stackoverflow.com/questions/60980409/separate-the-title-from-the-tabpanels-in-navbarpage
     bslib::bs_add_rules(
---
+++ R/round2.R
@@ -0,0 +1,22 @@
+#' Round numbers to specified number of digits
+#' This function is a drop-in replacement for `base::round()`
+#'
+#' Unlike `base::round()`, it does NOT follow the
+#'  IEC 60559 standard by rounding numbers ending in .5 to the even
+#'  digit. Instead, 0.5 is rounded up.
+#' @export
+#' @examples
+#' # This gives '2'
+#' base::round(2.5)
+#'
+#' # This gives 3
+#' round2(2.5)
+#' @source https://stackoverflow.com/questions/12688717/round-up-from-5
+round2 <- function(x, digits = 0) {
+  posneg <- sign(x)
+  z <- abs(x) * 10^digits
+  z <- z + 0.5 + sqrt(.Machine$double.eps)
+  z <- trunc(z)
+  z <- z / 10^digits
+  z * posneg
+}
