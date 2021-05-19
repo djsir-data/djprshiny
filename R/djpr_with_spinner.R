115104c679c68135613cd5d3e4458af2e68e65d3
Matt Cowgill <mattcowgill@gmail.com>
2021-05-19 15:30:00 +1000

    export djpr_with_spinner()

--- R/djpr_with_spinner.R
+++ R/djpr_with_spinner.R
@@ -1,3 +1,16 @@
+#' Wrapper around shinycssloaders::withSpinner() with DJPR defaults
+#'
+#' This function displays a spinner in place of Shiny UI elements while
+#' they are loading / refreshing
+#' @param ui_element An element such as `plotOutput()`
+#' @param type See \link{https://daattali.com/shiny/shinycssloaders-demo}
+#' @param colour Hex code for spinner
+#' @details Use within UI script
+#' @examples
+#' \dontrun{
+#' plotOutput("plot") %>% djpr_with_spinner()
+#' }
+#' @export
 
 djpr_with_spinner <- function(ui_element,
                               type = 8,
