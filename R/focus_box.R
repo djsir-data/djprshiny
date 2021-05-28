6bf17cbd14e6b5e4d6260ca95230d13cd2337837
Matt Cowgill <mattcowgill@gmail.com>
2021-05-28 15:31:10 +1000

    fix focus_box

--- R/focus_box.R
+++ R/focus_box.R
@@ -5,8 +5,7 @@
 #' @export
 
 focus_box <- function(...) {
-  fluidRow(
-    # #box-shadow: 3px 3px 2px #AEAEAE;
+  div(
     style = "padding-left: 15px; box-shadow: 0px 3px 1px -1px rgba(0, 0, 0, .2), 0px 0px 2px 0px rgba(0, 0, 0, 0.3), 0px 1px 3px 0px rgba(0, 0, 0, 0.12); border-radius: 6px; margin-top: 5px; margin-bottom: 5px; padding: 14px 20px 20px 20px;",
     ...
   )
