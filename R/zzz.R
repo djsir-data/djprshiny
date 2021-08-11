cd0664ff72aa25dd8901ce2d194052ddb5240a1c
Matt Cowgill <mattcowgill@gmail.com>
2021-08-12 09:45:01 +1000

    mem cache for girafe

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   addResourcePath("djprshiny", system.file("www", package = "djprshiny"))
 
-  djpr_shiny_cache <- cachem::cache_disk(
-    dir = file.path(dirname(tempdir()), "djpr-shiny-cache")
-  )
-  djpr_girafe_mem <<- memoise::memoise(djpr_girafe,
-                                       cache = djpr_shiny_cache)
+  djpr_girafe_mem <<- memoise::memoise(djpr_girafe)
 }
