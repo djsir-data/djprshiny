03340d910a773c3fe1192a30ebbb94493d15d852
Matt Cowgill <mattcowgill@gmail.com>
2021-08-18 10:11:19 +1000

    undo cache

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   addResourcePath("djprshiny", system.file("www", package = "djprshiny"))
 
-  djpr_girafe_mem <<- memoise::memoise(djpr_girafe,
-                                       cache = cachem::cache_disk())
+  djpr_girafe_mem <<- memoise::memoise(djpr_girafe)
 }
