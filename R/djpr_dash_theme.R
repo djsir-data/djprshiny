f6fda287b2f2adf7ae3f29b1fc084f0e3c3784f6
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-04-08 11:57:51 +1000

    removed old dashboard function

--- R/djpr_dash_theme.R
+++ R/djpr_dash_theme.R
@@ -1,36 +1,7 @@
-#' DJPR themed dashboard
-#'
-#' @param header shinydashboard dashboardHead
-#' @param sidebar shinydashboard dashboardSidebar
-#' @param body shinydashboard dashboardBody
-#' @param title temp
-#'
-#' @export
-
-djpr_dashboard <- function(header, sidebar, body, title = NULL){
-  shinydashboard::dashboardPage(
-    header = header,
-    sidebar = sidebar,
-    body = shiny::tagAppendChildren(
-      body,
-      shiny::tags$link(
-        rel = "stylesheet",
-        type = "text/css",
-        href = "djprshiny/dashboard.css"
-      ),
-      shiny::tags$script(
-        '$(".logo").prependTo(".main-header .navbar");
-        $(".sidebar-toggle").prependTo(".main-header .navbar");'
-        )
-      ),
-    title = title
-    )
-}
-
 #' DJPR dashboard theme
 #'
 #' @param ... unused
-#' @details add anywhere in a shinydashboard body to add DJPR theme and ggiraph
+#' @details add anywhere in a shinydashboard body to add DJPR theme
 #' javascript
 #' @export
 
@@ -44,7 +15,6 @@ djpr_dash_theme <- function(...){
     shiny::tags$script(
       '$(".logo").prependTo(".main-header .navbar");
         $(".sidebar-toggle").prependTo(".main-header .navbar");'
-    ),
-    ggiraph_js()
+    )
   )
 }
