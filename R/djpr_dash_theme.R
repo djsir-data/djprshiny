57251104da209fc8f03d58a2a64f47e58c88aa7f
Henry S McMillan (DJPR) <henry.mcmillan@ecodev.vic.gov.au>
2022-05-06 10:30:55 +1000

    rm ggirpah js from theme

--- R/djpr_dash_theme.R
+++ R/djpr_dash_theme.R
@@ -1,8 +1,7 @@
 #' DJPR dashboard theme
 #'
 #' @param ... unused
-#' @details add anywhere in a shinydashboard body to add DJPR theme and
-#' ggiraph_js()
+#' @details add anywhere in a shinydashboard body to add DJPR theme
 #' javascript
 #' @export
 
@@ -21,31 +20,7 @@ djpr_dash_theme <- function(...){
     shiny::tags$script(
       '$(".logo").prependTo(".main-header .navbar");
         $(".sidebar-toggle").prependTo(".main-header .navbar");'
-    ),
-    column(
-      12,
-      tags$body(
-        shiny::div(id = "ppitest", style = "width:0.75in;visible:hidden;padding:0px"),
-        shiny::fluidRow(
-          style = "visible: hidden",
-          column(12, id = "girafe_container"),
-        )
-      )
-      ),
-    tags$script(
-    '$(document).on("shiny:connected", function(e) {
-                                    var w = document.getElementById("girafe_container").offsetWidth;
-                                    var b = window.innerWidth;
-                                    var obj = {width: w, browser_width: b};
-                                    Shiny.onInputChange("plt_change", obj);
-                                });
-                                $(window).resize(function(e) {
-                                    var w = document.getElementById("girafe_container").offsetWidth;
-                                    var b = window.innerWidth;
-                                    var obj = {width: w, browser_width: b};
-                                    Shiny.onInputChange("plt_change", obj);
-                                });
-                            ')
+    )
     )
 
 }
