a8b7e80d54fc3dc53bca87bc599e9164193b3d0c
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-05-24 16:34:18 +1000

    heading boxes

---
+++ R/djpr_heading_box.R
@@ -0,0 +1,108 @@
+#' @name headings
+#' @aliases djpr_h1_box
+#' @aliases djpr_h2_box
+#' @aliases djpr_h3_box
+#' @aliases djpr_h4_box
+#'
+#' @title DJPR title boxes
+#'
+#' @param heading text for heading
+#' @param ... additional contents for the box
+#' @param width width of box on 1 to 12 scale (bootstrap grid)
+#' @param colour Colour for the box as css specification
+#' @param text_colour colour for text as css specification
+#'
+#' @return HTML element
+#' @export
+djpr_h1_box <- function(
+  heading,
+  ...,
+  width = 12,
+  colour = "#201547",
+  text_colour = "#FFFFFF"
+  ){
+  column(
+    width = width,
+    div(
+      class = "small-box",
+      div(
+        class = "inner",
+        style = paste0("background-color: ", colour, ";"),
+        h2(heading, style = paste0("color: ", text_colour, ";")),
+        ...
+      )
+
+    )
+  )
+}
+
+
+#' @rdname headings
+djpr_h2_box <- function(
+  heading,
+  ...,
+  width = 12,
+  colour = "#201547",
+  text_colour = "#FFFFFF"
+){
+  column(
+    width = width,
+    div(
+      class = "small-box",
+      div(
+        class = "inner",
+        style = paste0("background-color: ", colour, ";"),
+        h2(heading, style = paste0("color: ", text_colour, ";")),
+        ...
+      )
+
+    )
+  )
+}
+
+#' @rdname headings
+djpr_h3_box <- function(
+  heading,
+  ...,
+  width = 12,
+  colour = "#201547",
+  text_colour = "#FFFFFF"
+){
+  column(
+    width = width,
+    div(
+      class = "small-box",
+      div(
+        class = "inner",
+        style = paste0("background-color: ", colour, ";"),
+        h2(heading, style = paste0("color: ", text_colour, ";")),
+        ...
+      )
+
+    )
+  )
+}
+
+#' @rdname headings
+djpr_h4_box <- function(
+  heading,
+  ...,
+  width = 12,
+  colour = "#201547",
+  text_colour = "#FFFFFF"
+){
+  column(
+    width = width,
+    div(
+      class = "small-box",
+      div(
+        class = "inner",
+        style = paste0("background-color: ", colour, ";"),
+        h2(heading, style = paste0("color: ", text_colour, ";")),
+        ...
+      )
+
+    )
+  )
+}
+
